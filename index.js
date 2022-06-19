8832e19fc59b944d055f1474fc456476a9eee4d4
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-06-19 18:21:32 +0300

    commit on 'master'

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+    let x = [];
+    for (let i = 0; i < arr.length; i++) {
+        x.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+
+    }
+    return (x)
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown() {
+    var countdown = 10;
+    while (countdown > 0) {
+        console.log(countdown);
+        countdown = countdown - 1;
+    }
+    console.log(countdown);
+
+}
